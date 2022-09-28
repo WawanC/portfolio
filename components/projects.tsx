336211576228be2e57df39870df539d8dc5d0d5b
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2022-09-28 12:56:20 +0700

    add projects section

---
+++ components/projects.tsx
@@ -0,0 +1,24 @@
+const ProjectsData = [
+  { name: "Project A" },
+  { name: "Project B" },
+  { name: "Project C" },
+  { name: "Project D" },
+];
+
+const Projects = () => {
+  return (
+    <section className="flex flex-col py-24 justify-center items-center gap-8 border-b-2">
+      <h1 className="font-bold text-4xl">My Projects</h1>
+      <div className="border border-black flex p-4 gap-4 flex-wrap justify-center w-[80%]">
+        {ProjectsData.map((project) => (
+          <div className="flex flex-col justify-center items-center">
+            <div className="w-[300px] h-[200px] border border-black"></div>
+            <h1>{project.name}</h1>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import AboutMe from "../components/aboutme";
 import Hero from "../components/hero";
 import NavBar from "../components/navbar";
+import Projects from "../components/projects";
 import Techs from "../components/techs";
 
 const Home: NextPage = () => {
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
       <Hero />
       <AboutMe />
       <Techs />
+      <Projects />
     </>
   );
 };
