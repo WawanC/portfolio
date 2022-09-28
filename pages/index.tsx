ea401372ed30559d40af484cc57cc1501828eb87
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2022-09-28 11:20:16 +0700

    add tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1>Portfolio App</h1>;
+  return <h1 className="font-bold text-red-500">Portfolio App</h1>;
 };
 
 export default Home;
